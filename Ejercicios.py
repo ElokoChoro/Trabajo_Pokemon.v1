d4d4d6df620460b6d384ea1ada2ce906a8c6ec68
AvKuroro <117792104+AvKuroro@users.noreply.github.com>
2026-04-07 19:23:38 -0400

    Update Ejercicios.py

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,2 +1,9 @@
+#2
+
 print(df[df["Tipo 1"]== "Fuego"])
 print(df[["Nombre", "Tipo 1", "Ataque", "Velocidad"]])
+
+#3
+
+mayor_defensa = df[df["Defensa"] == df["Defensa"].max()]
+print(mayor_defensa)
