8794f49d4e679a929ea9480db59496c8ef4d03a9
ElokoChoro <javierdelgadocona@gmail.com>
2026-04-07 20:09:28 -0400

    ejercicios del 1 al 5

---
+++ Ejercicios.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ----------------------
+# 1. CARGA Y LIMPIEZA
+# ----------------------
+df = pd.read_csv("pokemon_primera_gen.csv")
+
+# ----------------------
+# 2. FILTRADO Y SELECCIÓN
+# ----------------------
+print(df[df["Tipo 1"]== "Fuego"])
+print(df[["Nombre", "Tipo 1", "Ataque", "Velocidad"]])
+
+# ----------------------
+# 3. ESTADÍSTICA DESCRIPTIVA
+# ----------------------
+print("promedio de ataque de todos los pokemones")
+print(df["Ataque"].mean())
+print("Mediana de ataque de todos los pokemons")
+print(df["Ataque"].median())
+print("Moda de ataque de todos los pokemones")
+print(df["Ataque"].mode())
+
+#cual es el pokemons con mayor defensa? y el de menor velocidad?
+print("El pokemon con mayor defensa es:")
+print(df.loc[df["Defensa"].idxmax()])
+print("El pokemon con menor velocidad es:")
+print(df.loc[df["Velocidad"].idxmin()])
+
+#¿Cuantos pokemons tienen 2 tipos?
+print("El numero de pokemons con 2 tipos es:")
+print(df[df["Tipo 2"].notna()].shape[0])
+
+#rango de los PS
+print("El rango de los PS es:")
+print(df["PS"].max() - df["PS"].min())
+
+#desviacion estandar de los PS
+print("La desviacion estandar de los PS es:")
+print(df["PS"].std())
+
+# ----------------------
+# 4. VISUALIZACIONES
+# ----------------------
+
+#histograma de los valores de ataque
+plt.figure()
+plt.hist(df["Ataque"], bins=15)
+plt.title("Histograma de Ataque")
+plt.show()
+
+#grafico de dispersion entre ataque y velocidad
+
+plt.figure()
+plt.scatter(df["Ataque"], df["Velocidad"])
+plt.xlabel("Ataque")
+plt.ylabel("Velocidad")
+plt.title("Grafico de dispersion entre Ataque y Velocidad")
+plt.show()
+
+#boxplot de los PS por tipo principal (TIPO 1)
+
+plt.figure()
+sns.boxplot(x="Tipo 1", y="PS", data=df)
+plt.xticks(rotation=45)
+plt.show()
+
+#distribucion de la defensa usando diagrama de violin
+
+plt.figure()
+sns.violinplot(y=df["Defensa"])
+plt.show()
+
+# ----------------------
+# 5. MANIPULACIÓN
+# ----------------------
+
